c87fc9181f6684982f0dbf99b6312c5d456d20a5
GatoSkate <ric.flores@duocuc.cl>
2024-05-29 12:10:39 -0400

    xd

--- galaxymusic/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Empleado
-
-class EmpleadoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Empleado
-        fields = ('__all__')
-
-
--- galaxymusic/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import EmpleadoList, EmpleadoDetail
-
-urlpatterns = [
-    path('empleado/', EmpleadoList.as_view()),
-    path('empleado/<int:pk>/', EmpleadoDetail.as_view()),
-
-
-]
